ea1394272dc3a1d7887bd52950410500f0a6aa43
Raji <rajbaymas@gmail.com>
2022-11-18 19:03:57 +0400

    initial commit

---
+++ Car_new/src/main/java/com/example/Car_new/CarNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.Car_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarNewApplication.class, args);
+	}
+
+}
---
+++ Car_new/src/test/java/com/example/Car_new/CarNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Car_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
